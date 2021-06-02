1475acfb2be646d84785b68ffaadfff0be067247
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2021-06-02 02:33:55 -0600

    control structure switch implemented and working

--- cminicompiler/src/main/java/test/factorial.c
+++ cminicompiler/src/main/java/test/factorial.c
@@ -1,4 +1,16 @@
 int funcionComoNo(){
-   if (1)return 1;else if(0);
-   int b = 111;
+   if(b>b)b++;else if(1){
+	};
+	switch(b^2 ? 4 : 1+2+3+4+'1' ? 1: 1){
+		case gravity^2 ? 4 : 1+2+3+4+'1' ? 1: 1:{
+			break;
+			break;
+		}break;
+		break;
+		case 0: case 1:
+		break;
+		
+		
+	}
+	switch(1)case 0: case 1:break;
 }
