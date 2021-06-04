d46d4e159605caa3e020a05897a63883a3b58913
josuerv99 <josuerojasvega@gmail.com>
2021-06-04 03:11:15 -0600

    .

--- cminicompiler/src/main/java/handler/Main.java
+++ cminicompiler/src/main/java/handler/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        String fileInputName = "src/main/java/test/program2.c";
+        String fileInputName = "src/main/java/test/test.c";
         String fileOutputName = "src/main/java/out/Ofactorial.xml";
 
         Analyzer analyzer = new Analyzer();
