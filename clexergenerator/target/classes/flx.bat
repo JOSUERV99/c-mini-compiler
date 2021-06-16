21e7ace6550239cd5d37d4d0f9f2dd2490a2a903
Josue Rojas Vega <31432526+JOSUERV99@users.noreply.github.com>
2021-06-16 01:45:57 -0600

    Delete flx.bat

--- clexergenerator/target/classes/flx.bat
+++
@@ -1,7 +0,0 @@
-:: EXECUTE JFLEX
-
-:: delete the last lexer generated
-del generated/CLexer.java /F /Q
-
-:: run jflex with some options, the .flex input file and dir name where the lexer will be generated
-java -jar "C:\\jflex-1.8.2\\lib\\jflex-full-1.8.2.jar" -d lexer --encoding utf-8 --time lexer/CLexerSpec.flex 
